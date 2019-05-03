e7252e3baba5652a1cb03faf31f23cd754a969a9
King0625 <a7164265@gmail.com>
2019-05-04 03:39:23 +0800

    Add edit and save mode

--- view/animation.js
+++ view/animation.js
@@ -1,3 +1,30 @@
 $(document).ready(() => {
-
+    var readonly = true;
+    var disabled = true;
+    $("#edit").click(function () {
+        $(this).css("background-color", "#fff4aa");
+        $("input[type='text']").attr('readonly', !readonly);
+        $("input[type='text']").css({
+            "background": "#f3f3f3",
+            "border-radius": "6px"
+        });
+        $("textarea").attr('readonly', !readonly);
+        $("textarea").css({
+            "background": "#f3f3f3",
+            "border-radius": "6px"
+        })
+        $("select").attr('disabled', !disabled);
+    });
+    $("#save").click(function () {
+        $("#edit").css("background-color", "#ffffff");
+        $("input[type='text']").attr('readonly', readonly);
+        $("input[type='text']").css({
+            "background": "#ffffff"
+        });
+        $("textarea").attr('readonly', readonly);
+        $("textarea").css({
+            "background": "#ffffff"
+        })
+        $("select").attr('disabled', disabled);
+    });
 });
