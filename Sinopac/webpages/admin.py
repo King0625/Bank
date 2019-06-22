f57ef1a078b81fb60091fe19cb32b132c8f05797
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-06-23 02:20:36 +0800

    make migration successful

--- Sinopac/webpages/admin.py
+++ Sinopac/webpages/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import BasicData, BankDepositData
+from .models import BasicData, BankDepositData,UnionCreditCheckSystemInfo, CreditCardInformation
 
 admin.site.register(BasicData)
 admin.site.register(BankDepositData)
-# admin.site.register(FDDData)
-# admin.site.register(UnionSearchData)
-# admin.site.register(CreditCardBasicData)
+admin.site.register(UnionCreditCheckSystemInfo)
+admin.site.register(CreditCardInformation)
 
 
 # Register your models here.
