20774ab7c9f79c33c990bee56bd82bdd4c2e1fe5
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-06-23 02:15:24 +0800

    finish sql design before migrate

--- Sinopac/webpages/models.py
+++ Sinopac/webpages/models.py
@@ -100,11 +100,11 @@ class BankDepositData(models.Model):
     # (八)客戶風險等級資訊與防制洗錢FDD系統查詢
     new_batch_processing_day = models.DateTimeField(null=True) #最新跑批日
 
-    VERY_LOW = 'VL'
-    LOW = 'L',
-    MIDDLE = 'M'
-    HIGH = 'H'
-    VERY_HIGH = 'VH'
+    # VERY_LOW = 'VL'
+    # LOW = 'L',
+    # MIDDLE = 'M'
+    # HIGH = 'H'
+    # VERY_HIGH = 'VH'
 
 
     # 洗錢風險分級
@@ -146,10 +146,10 @@ class UnionCreditCheckSystemInfo(models.Model):
 
     # Title information
     identity = models.CharField(max_length=15) #身分證字號
-    name = models.CharField(max_length=11)
-    eng_name = models.CharField(max_length=30)
-    birthday = models.DateField()
-    address = models.CharField(max_length=30)
+    name = models.CharField(max_length=11) # 姓名
+    eng_name = models.CharField(max_length=30) # 英文姓名
+    birthday = models.DateField() # 出生日期
+    address = models.CharField(max_length=30) # 戶籍地址
 
     other_info = models.CharField(max_length=15,default='N') # 其餘資訊
     repay_info = models.CharField(max_length=15,default='N') # 新增額度及每筆撥款清償資訊
@@ -167,7 +167,99 @@ class UnionCreditCheckSystemInfo(models.Model):
 
     # (一)通報案件紀錄資訊
 
+    # 1. 案件通報
+    case_notification_record = models.TextField(default='無')  # 
     
+    # 2. 遷徙公告
+    migration_record = models.TextField(default='無') #
+    
+    # 3. 身分證更改記錄
+    identity_modification_record = models.TextField(default='無') #
+
+    # 4. 補充註記資訊
+    supplement_comment_record = models.TextField(default='無') #
+
+    # 5. 國民身分證領補換資料查詢驗證
+    data_date = models.DateField()
+    identity_record = models.CharField(max_length=15)
+    reissue_code = models.IntegerField(default=0)
+    reissue_date = models.DateField()
+    birthdat_on_identity_system = models.DateField()
+    issue_place = models.CharField(max_length=15,default='台灣')
+
+    # 6. 在台無戶籍人士身分證資料
+    no_household_registration_in_TW_identity_information = models.TextField(default='無')
+    
+
+    
+    # (二) 授信保證資料
+    
+    data_date = models.DateField() # 資料日期
+    financial_product_conflict_comment = models.TextField(default='無') # 衍生性金融商品交易轉捩授信爭議案件住記 
+    joint_debt = models.TextField(default='查資料庫中無本人擔任他戶之共同債務資訊') # 共同債務 
+    debt = models.TextField(default='無')
+    other_information = models.TextField('無')
+    
+    # (三) 新增額度
+    new_quota = models.TextField(default='無')
+
+    # (四) 授信異常資料
+    overdue_collection_of_bad_debts = models.TextField(default='無') # 逾期催收及呆帳資料
+    transfer_of_overdue_information_claims = models.TextField(default='無') # 逾期資料債權轉讓
+    Personal_unincorporated_organizations_info = models.TextField(default='無') # 個人餘催呆授信留向非法人組織資訊
+
+    # (五) 票據退票及拒往資訊
+    data_info_date = models.DateField()
+
+    #(六) 信用卡資訊
+    data_process_date = models.DateField() # 資料處理日期 : 20190423
+    change_date = models.DateField() # 異動日期
+    orgnization = models.CharField(max_length=20) # 報送機構
+    ch_name = models.CharField(max_length=11) # 中文姓名
+    birthday_on_credit_system = models.DateField() # 出生日期
+    contact_address = models.TextField(default='') # 戶籍地址
+    regirster_address = models.TextField(default='') # 通訊地址
+    contact_phone_number = models.TextField(default='') # 居住電話
+    company_phone_number = models.TextField(default='') # 任職電話
+    company = models.TextField(default='') # 任職機構
+    job_title = models.TextField(default='') # 職稱
+    year_salary = models.IntegerField(default=0) # 年薪
+    career = models.IntegerField(default=0) # 年資
+    EDU_DEGREE = (
+        ('PHD', '博士'),
+        ('MASTER','碩士'),
+        ('BAC','學士'),
+        ('SH','高中'),
+        ('JH','國中'),
+        ('ES','國小'),
+        ('NONE','無')
+    )  
+    edu_degree = models.CharField(
+        max_length=5,
+        choices=EDU_DEGREE,
+        default = 'SH'
+    ) # 教育程度
+    owned_house = models.BooleanField(default=False) # 自用住宅有無
+
+    # (七)最近三個月被查詢記錄
+    new_business = models.IntegerField(default=0) # 新業務申請
+    original_business = models.IntegerField(default=0) # 原業務申請
+    new_and_original_business = models.IntegerField(default=0) # 原業務往來及新業務申請
+    other_financial_org_query_after_quered = models.IntegerField(default=0) # 查詢當日截至目前金融機構查詢次數
+    financial_query_time = models.IntegerField(default=0) # 金融機構查詢次數
+
+    # (八) 信用評分
+    score = models.IntegerField(default=0)
+
+class CreditCardInformation(models.Model):
+    identity = models.CharField(max_length=15,null=False)
+    name = models.CharField(max_length=11,null=False)
+
+    pass
+    
+    
+
+
 
 
 
