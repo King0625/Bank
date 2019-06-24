075f2362cb67c4420751e28431d4ad4c4ea86b1e
King0625 <a7164265@gmail.com>
2019-06-24 15:04:52 +0800

    Add alarm clock and adjust some js script

--- Sinopac/webpages/static/webpages/animation.js
+++ Sinopac/webpages/static/webpages/animation.js
@@ -208,8 +208,7 @@ $(document).ready(() => {
         $("#grid1-detail").toggleClass('shown');
         $("#grid1-detail").toggleClass('not-shown');
         $("#grid1-detail .close").click(function () {
-            $("#grid1-detail").toggleClass('shown');
-            $("#grid1-detail").toggleClass('not-shown');
+            $("#grid1-detail").addClass('not-shown');
         });
     });
 
@@ -219,8 +218,7 @@ $(document).ready(() => {
         $("#grid2-detail").toggleClass('shown');
         $("#grid2-detail").toggleClass('not-shown');
         $("#grid2-detail .close").click(function () {
-            $("#grid2-detail").toggleClass('shown');
-            $("#grid2-detail").toggleClass('not-shown');
+            $("#grid2-detail").addClass('not-shown');
         });
     });
 
@@ -228,8 +226,7 @@ $(document).ready(() => {
         $("#grid3-detail").toggleClass('shown');
         $("#grid3-detail").toggleClass('not-shown');
         $("#grid3-detail .close").click(function () {
-            $("#grid3-detail").toggleClass('shown');
-            $("#grid3-detail").toggleClass('not-shown');
+            $("#grid3-detail").addClass('not-shown');
         });
     });
 
@@ -237,8 +234,7 @@ $(document).ready(() => {
         $("#grid4-detail").toggleClass('shown');
         $("#grid4-detail").toggleClass('not-shown');
         $("#grid4-detail .close").click(function () {
-            $("#grid4-detail").toggleClass('shown');
-            $("#grid4-detail").toggleClass('not-shown');
+            $("#grid4-detail").addClass('not-shown');
         });
     });
 
@@ -246,8 +242,7 @@ $(document).ready(() => {
         $("#grid5-detail").toggleClass('shown');
         $("#grid5-detail").toggleClass('not-shown');
         $("#grid5-detail .close").click(function () {
-            $("#grid5-detail").toggleClass('shown');
-            $("#grid5-detail").toggleClass('not-shown');
+            $("#grid5-detail").addClass('not-shown');
         });
     });
 
@@ -255,64 +250,56 @@ $(document).ready(() => {
         $("#grid6-detail").toggleClass('shown');
         $("#grid6-detail").toggleClass('not-shown');
         $("#grid6-detail .close").click(function () {
-            $("#grid6-detail").toggleClass('shown');
-            $("#grid6-detail").toggleClass('not-shown');
+            $("#grid6-detail").addClass('not-shown');
         });
     });
     $("#grid7 .check").click(function () {
         $("#grid7-detail").toggleClass('shown');
         $("#grid7-detail").toggleClass('not-shown');
         $("#grid7-detail .close").click(function () {
-            $("#grid7-detail").toggleClass('shown');
-            $("#grid7-detail").toggleClass('not-shown');
+            $("#grid7-detail").addClass('not-shown');
         });
     });
     $("#grid8 .check").click(function () {
         $("#grid8-detail").toggleClass('shown');
         $("#grid8-detail").toggleClass('not-shown');
         $("#grid8-detail .close").click(function () {
-            $("#grid8-detail").toggleClass('shown');
-            $("#grid8-detail").toggleClass('not-shown');
+            $("#grid8-detail").addClass('not-shown');
         });
     });
     $("#grid9 .check").click(function () {
         $("#grid9-detail").toggleClass('shown');
         $("#grid9-detail").toggleClass('not-shown');
         $("#grid9-detail .close").click(function () {
-            $("#grid9-detail").toggleClass('shown');
-            $("#grid9-detail").toggleClass('not-shown');
+            $("#grid9-detail").addClass('not-shown');
         });
     });
     $("#grid10 .check").click(function () {
         $("#grid10-detail").toggleClass('shown');
         $("#grid10-detail").toggleClass('not-shown');
         $("#grid10-detail .close").click(function () {
-            $("#grid10-detail").toggleClass('shown');
-            $("#grid10-detail").toggleClass('not-shown');
+            $("#grid10-detail").addClass('not-shown');
         });
     });
     $("#grid11 .check").click(function () {
         $("#grid11-detail").toggleClass('shown');
         $("#grid11-detail").toggleClass('not-shown');
         $("#grid11-detail .close").click(function () {
-            $("#grid11-detail").toggleClass('shown');
-            $("#grid11-detail").toggleClass('not-shown');
+            $("#grid11-detail").addClass('not-shown');
         });
     });
     $("#grid12 .check").click(function () {
         $("#grid12-detail").toggleClass('shown');
         $("#grid12-detail").toggleClass('not-shown');
         $("#grid12-detail .close").click(function () {
-            $("#grid12-detail").toggleClass('shown');
-            $("#grid12-detail").toggleClass('not-shown');
+            $("#grid12-detail").addClass('not-shown');
         });
     });
     $("#grid13 .check").click(function () {
         $("#grid13-detail").toggleClass('shown');
         $("#grid13-detail").toggleClass('not-shown');
         $("#grid13-detail .close").click(function () {
-            $("#grid13-detail").toggleClass('shown');
-            $("#grid13-detail").toggleClass('not-shown');
+            $("#grid13-detail").addClass('not-shown');
         });
     });
 
--- Sinopac/webpages/static/webpages/toolbox.js
+++ Sinopac/webpages/static/webpages/toolbox.js
@@ -1,5 +1,6 @@
 var pin = true;
 var sum = false;
+
 //click event
 $(document).ready(function () {
     $(".draggable").draggable({ distance: 20 });
@@ -22,21 +23,7 @@ $(document).ready(function () {
 
 
     })
-    // $("#mark").click(function () {
-    //     $(".dataContent").append('<div class="draggable" ><div class="open"></div><div class="note"><select class="date"><option value="s">05/10(五)</option><option value="a">05/10(五)</option><option value="b">05/10(五)</option><option value="c">05/10(五)</option></select> <button id="close"></button><textarea></textarea><input id="markedit" type="button" value="儲存"><input id="delete" type="button" value="刪除"></div></div>');
-    //     $("#close").on("click", function () { $(this).parent().animate({ opacity: 0 }, 500); });
-    //     $(".draggable .open").on("click", function () {
-    //         if ($(this).next().css("opacity") == 1) { $(this).next().animate({ opacity: 0 }, 500); }
-    //         else { $(this).next().animate({ opacity: 1 }, 500); }
 
-    //     });
-    //     $(".draggable #markedit").on("click", function () {
-    //         $(this).parent().animate({ opacity: 0 }, 500);
-
-    //     });
-    //     $(".draggable").draggable({ distance: 20 });
-
-    // })
     $("#sum").click(function () {
         if (!sum) {
             $("#summary").fadeIn();
@@ -60,8 +47,12 @@ $(document).ready(function () {
     })
     $("#alert-save").click(function () {
         $("textarea").attr('readonly', true);
-
-    })
+        $("#alarm").append("<div class='alert-list'><h3><img src='../../static/webpages/img/alarm.png' width='30px;' ><span class='alert-note'>" + $("#alert-summary").val() + "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + $("#hour").val() + ":" + $("#minute").val() + "</span><button class='alert-list-close'><img src='../../static/webpages/img/baseline-clear-24px.png'></button></h3> ")
+        $(".alert-list-close").on("click", function () {
+            $(this).parents(".alert-list").fadeOut();
+            $(this).parents(".alert-list").remove();
+        });
+    });
 
     $(".alert .close").click(function () {
         $(".note").fadeOut();
